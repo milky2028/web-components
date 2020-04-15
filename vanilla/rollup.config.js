d5c1de9eef2e05e7280b8c220d43bfeea730d972
Tyler <tylergross28@gmail.com>
2020-04-15 12:19:04 -0500

    basic build

---
+++ vanilla/rollup.config.js
@@ -0,0 +1,28 @@
+import rimraf from 'rimraf';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+rimraf.sync('dist');
+
+const plugins = [
+  typescript(),
+  terser({
+    output: {
+      comments: false
+    }
+  })
+];
+
+export default [
+  {
+    input: {
+      main: 'src/counter.ts'
+    },
+    output: {
+      dir: 'dist',
+      format: 'esm',
+      entryFileNames: '[name]-[hash].js'
+    },
+    plugins
+  }
+];

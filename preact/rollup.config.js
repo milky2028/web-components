d0e84548c84cfcd9511c3d5a2a962d6a25bc8e91
Tyler <tylergross28@gmail.com>
2020-04-21 10:58:53 -0500

    use babel to transpile

--- preact/rollup.config.js
+++ preact/rollup.config.js
@@ -3,24 +3,45 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 rimraf.sync('../public/preact');
 
 export default ({ watch }) => {
   const extensions = ['.js', '.jsx', '.ts', '.tsx'];
-  const plugins = [resolve({ extensions }), commonjs(), typescript()];
+  const plugins = [
+    resolve({ extensions }),
+    commonjs(),
+    typescript(),
+    babel({
+      extensions,
+      exclude: ['node_modules/**'],
+      plugins: [
+        [
+          '@babel/plugin-transform-react-jsx',
+          { pragma: 'h', pragmaFrag: 'Fragment' }
+        ]
+      ],
+      runtimeHelpers: true
+    })
+  ];
 
   if (watch) {
     plugins.unshift(
       replace({
         [`// __preactDebug__`]: `import 'preact/debug';`,
+        ['process.env.NODE_ENV']: `'development'`,
         delimiters: ['', '']
       })
     );
   } else {
     plugins.unshift(
-      replace({ [`// __preactDebug__`]: '', delimiters: ['', ''] })
+      replace({
+        [`// __preactDebug__`]: '',
+        ['process.env.NODE_ENV']: `'production'`,
+        delimiters: ['', '']
+      })
     );
 
     plugins.push(

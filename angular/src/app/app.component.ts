22fdb3d661912410d1f6f16dba96eb2a88a376c6
Tyler <tylergross28@gmail.com>
2020-04-21 16:45:22 -0500

    move definition

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private injector: Injector) {
+    const webComponent = createCustomElement(CounterComponent, {
+      injector: this.injector
+    });
+    customElements.define('angular-counter', webComponent);
+  }
+}
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
-import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [AppComponent, CounterComponent],
@@ -12,11 +11,4 @@ import { createCustomElement } from '@angular/elements';
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {
-  constructor(private injector: Injector) {
-    const webComponent = createCustomElement(CounterComponent, {
-      injector: this.injector
-    });
-    customElements.define('angular-counter', webComponent);
-  }
-}
+export class AppModule {}

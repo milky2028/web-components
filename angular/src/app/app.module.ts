3136fbfd82902c1817ba2b0c82ac01c019aadaef
Tyler <tylergross28@gmail.com>
2020-04-21 16:18:16 -0500

    angular base stuff

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CounterComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const webComponent = createCustomElement(CounterComponent, {
+      injector: this.injector
+    });
+    customElements.define('angular-counter', webComponent);
+  }
+}
--- angular/src/app/counter/counter.component.ts
+++ angular/src/app/counter/counter.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-counter',
+  selector: 'app-angular-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })

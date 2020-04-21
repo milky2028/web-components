ef7a03f099daeff245554b6788971151de10399e
Tyler <tylergross28@gmail.com>
2020-04-21 17:02:43 -0500

    vue base

---
+++ vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Counter />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Counter from './components/Counter.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Counter
+  }
+});
+</script>
---
+++ vue/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+    <p>Count: {{ count }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+button {
+  background-color: blueviolet;
+  color: white;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {
+    const count = ref(0);
+    const increment = () => count.value++;
+    const decrement = () => count.value--;
+
+    return { count, increment, decrement };
+  }
+});
+</script>

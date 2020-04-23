0fdf5c30422340d525a0f698b7032269e3631bda
Tyler <tylergross28@gmail.com>
2020-04-23 16:22:45 -0500

    edit master build config

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cd ..
 cd vanilla && rm -rf node_modules && yarn && yarn build
 cd ..
 
-cd angular && rm -rf node_modules && yarn && yarn build --prod --named-chunks
+cd angular && rm -rf node_modules && yarn && yarn build --prod --single-bundle true
 cd ..
 
 cd vue && rm -rf node_modules && yarn && yarn build --target wc-async --inline-vue src/components/*.vue
